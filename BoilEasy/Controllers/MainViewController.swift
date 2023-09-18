2fef99ab96167528c40e7a6bec78516ec20870af
MK1 <shin.m.dp@gmail.com>
2023-09-18 21:37:55 +0300

    update 1.2

--- BoilEasy/Controllers/MainViewController.swift
+++ BoilEasy/Controllers/MainViewController.swift
@@ -1,120 +1,58 @@
-////
-////  MainViewController.swift
-////  BoilEasy
-////
-////  Created by SHIN MIKHAIL on 18.09.2023.
-////
 //
-//import Foundation
-//import UIKit
-//import SnapKit
+//  MainViewController.swift
+//  BoilEasy
 //
-//final class MainViewController: UIViewController {
+//  Created by SHIN MIKHAIL on 18.09.2023.
 //
-//    var labels = [UILabel]()
-//    var currentIndex = 1
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        createLabels()
-//
-//        // Добавьте жесты для свайпа влево и вправо
-//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-//        swipeLeft.direction = .left
-//        view.addGestureRecognizer(swipeLeft)
-//
-//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-//        swipeRight.direction = .right
-//        view.addGestureRecognizer(swipeRight)
-//    }
-//
-//    func createLabels() {
-//        for i in 0..<3 {
-//            let label = UILabel()
-//            label.text = "Label \(i + 1)"
-//            label.textAlignment = .center
-//            view.addSubview(label)
-//            labels.append(label)
-//
-//            // Используйте SnapKit для размещения лейблов
-//            label.snp.makeConstraints { make in
-//                make.width.equalTo(view)
-//                make.height.equalTo(50)
-////                make.centerX.equalTo(view).offset(CGFloat(i - currentIndex) * view.frame.width)
-//                make.centerX.equalTo(view).offset(CGFloat(i - currentIndex) * (view.frame.width / 3)) // Уменьшаем смещение в 2 раза
-//
-//                make.centerY.equalTo(view).offset(100)
-//            }
-//        }
-//    }
-//
-//    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
-//        switch gesture.direction {
-//        case .left:
-//            currentIndex = (currentIndex + 1) % labels.count
-//        case .right:
-//            currentIndex = (currentIndex - 1 + labels.count) % labels.count
-//        default:
-//            break
-//        }
-//
-//        animateLabels()
-//    }
-//
-//    func animateLabels() {
-//        UIView.animate(withDuration: 0.3) {
-//            for (index, label) in self.labels.enumerated() {
-//                label.snp.updateConstraints { make in
-//                    make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * self.view.frame.width)
-//                }
-//            }
-//            self.view.layoutIfNeeded()
-//        }
-//    }
-//}
-
 import UIKit
 import SnapKit
 
 final class MainViewController: UIViewController {
-
-    var labels = [UILabel]()
-    var currentIndex = 1
-
+    //MARK: Properties
+    private let labels = ["Soft", "Medium", "Hard"]
+    private var currentIndex = 1
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .darkGray
         createLabels()
-
-        // Добавьте жесты для свайпа влево и вправо
+        gestures()
+    }
+    //MARK: Methods
+    private func gestures() {
+        // тап
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+        // жесты для свайпа влево
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeLeft.direction = .left
         view.addGestureRecognizer(swipeLeft)
-
+        // жесты для свайпа вправо
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeRight.direction = .right
         view.addGestureRecognizer(swipeRight)
     }
-
-    func createLabels() {
-        for i in 0..<3 {
+    
+    private func createLabels() {
+        for (index, labelText) in labels.enumerated() {
             let label = UILabel()
-            label.text = "Label \(i + 1)"
+            label.text = labelText
             label.textAlignment = .center
+            label.tag = index // Добавьте тег для каждого лейбла
+            
             view.addSubview(label)
-            labels.append(label)
-
             label.snp.makeConstraints { make in
                 make.width.equalTo(view)
                 make.height.equalTo(50)
-                make.centerX.equalTo(view).offset(CGFloat(i - currentIndex) * (view.frame.width / 3))
+                make.centerX.equalTo(view).offset(CGFloat(index - currentIndex) * (view.frame.width / 3))
                 make.centerY.equalTo(view).offset(100)
             }
+            // Установите начальное состояние (активный/неактивный) для каждого лейбла
+            label.textColor = index == currentIndex ? .white : .gray
         }
     }
-
-    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+    
+    @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
         switch gesture.direction {
         case .left:
             currentIndex = (currentIndex + 1) % labels.count
@@ -123,15 +61,30 @@ final class MainViewController: UIViewController {
         default:
             break
         }
-
         animateLabels()
     }
-
-    func animateLabels() {
+    
+    @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
+        let tapLocation = gesture.location(in: view)
+        // Проверьте, на какой лейбл был сделан тап и переключитесь на него, если он неактивен
+        for (index, label) in view.subviews.enumerated() {
+            if let label = label as? UILabel, label.frame.contains(tapLocation), label.tag != currentIndex {
+                currentIndex = label.tag
+                animateLabels()
+                break
+            }
+        }
+    }
+    
+    private func animateLabels() {
         UIView.animate(withDuration: 0.3) {
-            for (index, label) in self.labels.enumerated() {
-                label.snp.updateConstraints { make in
-                    make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * (self.view.frame.width / 3))
+            for (index, label) in self.view.subviews.enumerated() {
+                if let label = label as? UILabel {
+                    label.snp.updateConstraints { make in
+                        make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * (self.view.frame.width / 3))
+                    }
+                    // Обновите цвет текста в зависимости от активности лейбла
+                    label.textColor = index == self.currentIndex ? .white : .lightGray
                 }
             }
             self.view.layoutIfNeeded()
--- BoilEasy/Controllers/text.swift
+++ BoilEasy/Controllers/text.swift
@@ -1,83 +1,81 @@
-////
-////  text.swift
-////  BoilEasy
-////
-////  Created by SHIN MIKHAIL on 18.09.2023.
-////
-//
-//import Foundation
-////
-////  MainViewController.swift
-////  BoilEasy
-////
-////  Created by SHIN MIKHAIL on 18.09.2023.
-////
-//
-//import Foundation
-//import UIKit
-//import SnapKit
-//
-//final class MainViewController: UIViewController {
-//
-//    var labels = [UILabel]()
-//    var currentIndex = 1
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        createLabels()
-//
-//        // Добавьте жесты для свайпа влево и вправо
-//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-//        swipeLeft.direction = .left
-//        view.addGestureRecognizer(swipeLeft)
-//
-//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-//        swipeRight.direction = .right
-//        view.addGestureRecognizer(swipeRight)
-//    }
-//
-//    func createLabels() {
-//        for i in 0..<3 {
-//            let label = UILabel()
-//            label.text = "Label \(i + 1)"
-//            label.textAlignment = .center
-//            view.addSubview(label)
-//            labels.append(label)
-//
-//            // Используйте SnapKit для размещения лейблов
-//            label.snp.makeConstraints { make in
-//                make.width.equalTo(view)
-//                make.height.equalTo(50)
-////                make.centerX.equalTo(view).offset(CGFloat(i - currentIndex) * view.frame.width)
-//                make.centerX.equalTo(view).offset(CGFloat(i - currentIndex) * (view.frame.width / 3)) // Уменьшаем смещение в 2 раза
-//
-//                make.centerY.equalTo(view).offset(100)
-//            }
-//        }
-//    }
-//
-//    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
-//        switch gesture.direction {
-//        case .left:
-//            currentIndex = (currentIndex + 1) % labels.count
-//        case .right:
-//            currentIndex = (currentIndex - 1 + labels.count) % labels.count
-//        default:
-//            break
-//        }
-//
-//        animateLabels()
-//    }
-//
-//    func animateLabels() {
-//        UIView.animate(withDuration: 0.3) {
-//            for (index, label) in self.labels.enumerated() {
-//                label.snp.updateConstraints { make in
-//                    make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * self.view.frame.width)
-//                }
-//            }
-//            self.view.layoutIfNeeded()
-//        }
-//    }
-//}
+/*
+import UIKit
+import SnapKit
+
+final class MainViewController: UIViewController {
+    var labels = ["Soft", "Medium", "Hard"]
+    var currentIndex = 1
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        createLabels()
+        swipeGesture()
+    }
+
+    private func swipeGesture() {
+        // Добавьте жесты для свайпа влево и вправо
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+    }
+
+    private func createLabels() {
+        for (index, labelText) in labels.enumerated() {
+            let label = UILabel()
+            label.text = labelText
+            label.textAlignment = .center
+            view.addSubview(label)
+
+            label.snp.makeConstraints { make in
+                make.width.equalTo(view)
+                make.height.equalTo(50)
+                make.centerX.equalTo(view).offset(CGFloat(index - currentIndex) * (view.frame.width / 3))
+                make.centerY.equalTo(view).offset(100)
+            }
+        }
+    }
+
+    @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+        switch gesture.direction {
+        case .left:
+            currentIndex = (currentIndex + 1) % labels.count
+        case .right:
+            currentIndex = (currentIndex - 1 + labels.count) % labels.count
+        default:
+            break
+        }
+
+        animateLabels()
+    }
+
+    private func animateLabels() {
+        UIView.animate(withDuration: 0.3) {
+            for (index, label) in self.view.subviews.enumerated() {
+                if let label = label as? UILabel {
+                    label.snp.updateConstraints { make in
+                        make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * (self.view.frame.width / 3))
+                    }
+                }
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+}
+*/
+
+
+
+/*
+Мягкие вареные яйца (Soft-Boiled Eggs):
+Мягко-вареное яйцо с жидким желтком: 4-5 минут.
+Мягко-вареное яйцо с полужидким желтком: 6-7 минут.
+Средне-мягкие вареные яйца (Medium-Boiled Eggs):
+Средне-мягкое яйцо с немного жидким центром: 8-9 минут.
+Твердые вареные яйца (Hard-Boiled Eggs):
+Твердое яйцо с полностью застывшим желтком: 10-12 минут.
+*/
