9af3b6c605e7f79a6d37c60901d104c94493f370
MK1 <shin.m.dp@gmail.com>
2023-09-18 21:42:18 +0300

    update 1.3

--- BoilEasy/Controllers/MainViewController.swift
+++ BoilEasy/Controllers/MainViewController.swift
@@ -11,47 +11,63 @@ final class MainViewController: UIViewController {
     //MARK: Properties
     private let labels = ["Soft", "Medium", "Hard"]
     private var currentIndex = 1
+
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
         createLabels()
-        gestures()
+        setupGestures()
     }
+
     //MARK: Methods
-    private func gestures() {
-        // тап
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        view.addGestureRecognizer(tapGesture)
-        // жесты для свайпа влево
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-        swipeLeft.direction = .left
-        view.addGestureRecognizer(swipeLeft)
-        // жесты для свайпа вправо
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
-        swipeRight.direction = .right
-        view.addGestureRecognizer(swipeRight)
+
+    // Функция для создания лейбла
+    private func createLabel(text: String, tag: Int) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textAlignment = .center
+        label.tag = tag
+        return label
     }
     
     private func createLabels() {
         for (index, labelText) in labels.enumerated() {
-            let label = UILabel()
-            label.text = labelText
-            label.textAlignment = .center
-            label.tag = index // Добавьте тег для каждого лейбла
-            
-            view.addSubview(label)
-            label.snp.makeConstraints { make in
-                make.width.equalTo(view)
-                make.height.equalTo(50)
-                make.centerX.equalTo(view).offset(CGFloat(index - currentIndex) * (view.frame.width / 3))
-                make.centerY.equalTo(view).offset(100)
-            }
-            // Установите начальное состояние (активный/неактивный) для каждого лейбла
+            let label = createLabel(text: labelText, tag: index)
+            setupLabelConstraints(label, index: index)
             label.textColor = index == currentIndex ? .white : .gray
         }
     }
-    
+
+    // Функция для создания и установки констрейнтов для лейбла
+    private func setupLabelConstraints(_ label: UILabel, index: Int) {
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.width.equalTo(view)
+            make.height.equalTo(50)
+            make.centerX.equalTo(view).offset(CGFloat(index - currentIndex) * (view.frame.width / 3))
+            make.centerY.equalTo(view).offset(100)
+        }
+    }
+
+
+
+    private func setupGestures() {
+        // Тап
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGesture)
+
+        // Жесты для свайпа влево
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+
+        // Жесты для свайпа вправо
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+    }
+
     @objc private func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
         switch gesture.direction {
         case .left:
@@ -63,10 +79,9 @@ final class MainViewController: UIViewController {
         }
         animateLabels()
     }
-    
+
     @objc private func handleTap(_ gesture: UITapGestureRecognizer) {
         let tapLocation = gesture.location(in: view)
-        // Проверьте, на какой лейбл был сделан тап и переключитесь на него, если он неактивен
         for (index, label) in view.subviews.enumerated() {
             if let label = label as? UILabel, label.frame.contains(tapLocation), label.tag != currentIndex {
                 currentIndex = label.tag
@@ -75,7 +90,7 @@ final class MainViewController: UIViewController {
             }
         }
     }
-    
+
     private func animateLabels() {
         UIView.animate(withDuration: 0.3) {
             for (index, label) in self.view.subviews.enumerated() {
@@ -83,8 +98,7 @@ final class MainViewController: UIViewController {
                     label.snp.updateConstraints { make in
                         make.centerX.equalTo(self.view).offset(CGFloat(index - self.currentIndex) * (self.view.frame.width / 3))
                     }
-                    // Обновите цвет текста в зависимости от активности лейбла
-                    label.textColor = index == self.currentIndex ? .white : .lightGray
+                    label.textColor = index == self.currentIndex ? .white : .gray
                 }
             }
             self.view.layoutIfNeeded()
